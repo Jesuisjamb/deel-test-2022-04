2fff1715107e90501f0ae702ae17850d859f6ada
jesuisjamb <jesuisjamb@gmail.com>
2022-04-17 17:35:22 -0400

    ContractApi-1 --> Code route, controller, service

---
+++ src/routes/contractRoutes.js
@@ -0,0 +1,16 @@
+const {getProfile} = require('../middleware/getProfile')
+const contractController = require('../controllers/contractController')
+const express = require('express');
+const response = express.response;
+const router = express.Router();
+
+// 1. ***GET*** `/contracts/:id` - This API is broken 😵! it should return the contract only if it belongs to the profile calling. better fix that!
+router.get('/:id', getProfile, async (req, res = response) =>{
+    const contract = await contractController.getById(req)
+    if(!contract) return res.status(404).json({'msg':'Error'}).end()
+    res.json({contract})
+})
+
+// 1. ***GET*** `/contracts` - Returns a list of contracts belonging to a user (client or contractor), the list should only contain non terminated contracts.  
+
+module.exports = router;
---
+++ src/service/contractService.js
@@ -0,0 +1,12 @@
+exports.findOne = async (params) =>{
+    const {model, id, profileType, profileId} = params
+    const {Contract} = model
+    const queryObj = {
+        'contractor': {id, ContractorId: profileId},
+        'client'    : {id, ClientId: profileId}
+    }
+
+    return await Contract.findOne({
+        where: queryObj[profileType],
+    })
+}
