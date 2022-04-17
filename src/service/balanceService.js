9bca3ae9d14a5af2ee05717058c6fe96bf367b32
jesuisjamb <jesuisjamb@gmail.com>
2022-04-17 18:37:24 -0400

    BalanceApi-1 --> Add service transactions

--- src/service/balanceService.js
+++ src/service/balanceService.js
@@ -19,4 +19,19 @@ exports.handleBalance = async (params) =>{
             required: true,
         }],
     })
+    .then(async (contractData) => {
+        if(!contractData[0]) throw('Empty object')
+        
+        const totalJobs  = parseFloat(contractData[0]['Jobs.total'])
+        const clientId   = contractData[0]['ClientId']
+        const newBalance = parseFloat((totalJobs)*0.25).toFixed(2)
+        
+        // Transaction
+        await Profile.update({ balance: sequelize.literal(`balance + ${newBalance}`)}, {where : { id: clientId }})
+        return {'msg':'Balance added'}
+    })
+    .catch(e => {
+        console.error(e)
+        return
+    })
 }
